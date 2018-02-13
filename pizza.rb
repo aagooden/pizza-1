2c6d5a40566234b555d250ab5f2bbb3200dca199
Rob Bird <robertbirdwv@gmail.com>
2018-02-13 15:26:17 -0500

    Added extra cheese, double pepperoni.

--- pizza.rb
+++ pizza.rb
@@ -1,12 +1,20 @@
 crusts = ["regular", "stuffed", "pretzel", "bacon wrapped"] #array of crusts
 meats = ["sausage", "pepperoni", "bacon", "ham", "beef"] #array of meats
 veggies = ["peppers", "banana peppers", "olives", "pineapple"]#array of veggies
-sauce = ["regular", "cheese",]#array of sauces
-special = ["mushrooms", ]#array of special toppings
-#boolean double cheese
-#cheese = #random boolean
-#boolean double pepperoni
-#pepperoni = #random boolean
+sauce = ["regular", "cheese"]#array of sauces
+special = ["mushrooms", "three cheese blend"]#array of special toppings
+
+def cheese
+  if [true, false].sample == true
+    "extra cheese, "
+  end
+end
+
+def double_pepperoni
+  if [true, false].sample == true
+    "double pepperoni, "
+  end
+end
 
 #ask user how many pizzas
 puts "How many pizzas would you like?"
@@ -16,5 +24,5 @@ puts "Total cost for #{quantity} pizzas is $#{quantity * 12.99.to_f}."
 
 while quantity > 0 do
   quantity -= 1
-  puts "Pizza with #{meats.sample}, #{veggies.sample}, #{sauce.sample} sauce, and #{crusts.sample} crust."
+  puts "Pizza with #{double_pepperoni}#{meats.sample}, #{veggies.sample}, #{special.sample}, #{cheese}#{sauce.sample} sauce, and #{crusts.sample} crust."
 end
