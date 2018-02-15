d9acc463a156e401bbda412d5d4af5d9009d0edc
Rob Bird <robertbirdwv@gmail.com>
2018-02-15 14:14:37 -0500

    Added tip, fixed total.

--- pizza.rb
+++ pizza.rb
@@ -5,17 +5,15 @@ sauce = ["regular", "cheese", "ranch", "BBQ"]
 special = ["three cheese blend", "toothpaste", "legos"]
 size = ["Small", "Medium", "Large", "Extra large"]
 
-def cheese(cost)
+def cheese()
   if [true, false].sample == true
     return "extra cheese, "
-    cost += 1
   end
 end
 
-def double_pepperoni(cost)
+def double_pepperoni()
   if [true, false].sample == true
     return "double pepperoni, "
-    cost += 1
   end
 end
 
@@ -31,12 +29,12 @@ def size_cost(size)
   end
 end
 
-#ingredients_cost(array, cost)
+#ingredient_cost(array, cost)
 
 #end
 
 puts "How many pizzas would you like?"
-quantity = $stdin.gets.chomp.to_i
+quantity = gets.chomp.to_i
 
 cost = 0.00.to_f #subtotal
 tax_rate = 0.06
@@ -48,15 +46,14 @@ while counter < quantity do
   sizeofpizza = size.sample
   size_cost(sizeofpizza)
 
-  puts "# #{counter}:  #{sizeofpizza} pizza with with #{double_pepperoni(cost)}#{meats.sample}, #{veggies.sample}, #{special.sample}, #{cheese(cost)}#{sauce.sample} sauce, and #{crusts.sample} crust."
+  puts "# #{counter}:  #{sizeofpizza} pizza with with #{double_pepperoni}#{meats.sample}, #{veggies.sample}, #{special.sample}, #{cheese}#{sauce.sample} sauce, and #{crusts.sample} crust."
   cost = cost + size_cost(sizeofpizza).to_f
 end
 puts "\n"
 
-#Fees go here (i.e. delivery)
 delivery_fee = quantity * 2
-puts "Delivery Fee: $#{sprintf("%.02f", delivery_fee}"
+puts "Delivery Fee: $#{sprintf("%.02f", delivery_fee)}"
 puts "Subtotal: $#{sprintf("%.02f", cost)}"
 puts "Tax: $#{sprintf("%.02f", cost * tax_rate)}"#tax
-puts "Total cost is $#{sprintf("%.02f", cost)}"
-#puts "\nSuggested Tip: #{sprintf("%.02f", cost * 0.07)}"
+puts "Total cost is $#{sprintf("%.02f", cost * (1 + tax_rate))}"
+puts "\nSuggested Tip: #{sprintf("%.02f", cost * 0.10)}"
