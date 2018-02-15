801c068f7c78e66d3343917453415d0d4e8bb836
Rob Bird <robertbirdwv@gmail.com>
2018-02-15 08:51:57 -0500

    Size prices added; not working as intended.

--- pizza.rb
+++ pizza.rb
@@ -1,8 +1,8 @@
-crusts = ["regular", "stuffed", "pretzel", "bacon wrapped"] #array of crusts
-meats = ["sausage", "pepperoni", "bacon", "ham", "beef"] #array of meats
-veggies = ["peppers", "banana peppers", "olives", "pineapple"]#array of veggies
-sauce = ["regular", "cheese"]#array of sauces
-special = ["mushrooms", "three cheese blend"]#array of special toppings
+crusts = ["regular", "stuffed", "pretzel", "bacon wrapped"]
+meats = ["sausage", "pepperoni", "bacon", "ham", "beef"]
+veggies = ["peppers", "banana peppers", "olives", "pineapple"]
+sauce = ["regular", "cheese", "ranch", "BBQ"]
+special = ["mushrooms", "three cheese blend"]
 
 def cheese
   if [true, false].sample == true
@@ -12,17 +12,32 @@ end
 
 def double_pepperoni
   if [true, false].sample == true
+    $total_cost += 1
     "double pepperoni, "
   end
 end
 
-#ask user how many pizzas
+def size_selection
+  size = ["Small", "Medium", "Large", "Extra large"]
+  return size.sample #Note:  returned string is displayed properly, but not total cost
+  if size[1] #if larger than small (index 0) add 1 for each size larger
+    $total_cost += 1
+  elsif size[2]
+    $total_cost += 2
+  elsif size[3]
+    $total_cost += 3
+  end
+end
+
 puts "How many pizzas would you like?"
 quantity = $stdin.gets.chomp.to_i
 
-puts "Total cost for #{quantity} pizzas is $#{quantity * 16.99.to_f}."
+$total_cost = quantity * 14.99
 
-while quantity > 0 do
-  quantity -= 1
-  puts "Pizza with #{double_pepperoni}#{meats.sample}, #{veggies.sample}, #{special.sample}, #{cheese}#{sauce.sample} sauce, and #{crusts.sample} crust."
+bake_pizza = quantity
+
+while bake_pizza > 0 do
+  bake_pizza -= 1
+  puts "#{size_selection} pizza with #{double_pepperoni}#{meats.sample}, #{veggies.sample}, #{special.sample}, #{cheese}#{sauce.sample} sauce, and #{crusts.sample} crust."
 end
+print "#{quantity} pizzas at #{$total_cost}."
