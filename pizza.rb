1f2bc936607f28686b0c0ebfc9d6cb9145ce5dd6
Rob Bird <robertbirdwv@gmail.com>
2018-02-15 13:14:36 -0500

    Delivery fee with decimal point.

--- pizza.rb
+++ pizza.rb
@@ -20,7 +20,7 @@ def double_pepperoni(cost)
 end
 
 def size_cost(size)
-  if size == "Small"
+  if size[0]
     return 10.99
   elsif size[1]
     return 12.99
@@ -55,8 +55,8 @@ puts "\n"
 
 #Fees go here (i.e. delivery)
 delivery_fee = quantity * 2
-puts "Delivery Fee: $#{delivery_fee}"
+puts "Delivery Fee: $#{sprintf("%.02f", delivery_fee}"
 puts "Subtotal: $#{sprintf("%.02f", cost)}"
-puts "Tax: $#{sprintf("%0.02f", cost * tax_rate)}"#tax
+puts "Tax: $#{sprintf("%.02f", cost * tax_rate)}"#tax
 puts "Total cost is $#{sprintf("%.02f", cost)}"
 #puts "\nSuggested Tip: #{sprintf("%.02f", cost * 0.07)}"
