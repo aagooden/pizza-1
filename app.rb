e73919872a304399d3134fa3e874681df356b3e2
Rob Bird <robertbirdwv@gmail.com>
2018-04-05 15:58:29 -0400

    Iterating through hash to display sides & their prices with radio select.

--- app.rb
+++ app.rb
@@ -4,9 +4,20 @@ require_relative 'pizza.rb'
 enable :sessions
 
 get '/' do
+  session[:master_order] = Array.new
   erb :index
 end
 
 get '/menu' do
   erb :menu
+end
+
+get '/sides' do
+  menu = menu_return
+  erb :sides, locals: {menu:menu}
+end
+
+post '/add_to_order' do
+  add_to_order(params[:selection], session[:master_order])
+  redirect '/menu'
 end
--- pizza.rb
+++ pizza.rb
@@ -1,63 +1,14 @@
-crusts = ["regular", "stuffed", "pretzel", "bacon wrapped"]
-meats = ["sausage", "pepperoni", "bacon", "ham", "beef"]
-veggies = ["peppers", "banana peppers", "olives", "pineapple", "mushrooms"]
-sauce = ["regular", "cheese", "ranch", "BBQ"]
-special = ["three cheese blend", "toothpaste", "legos"]
-size = ["Small", "Medium", "Large", "Extra large"]
-
-order = []
-
-=begin
-
- Nest hashes of hashes. Iterate over it in erb to display options; use this for multiple pages. Start with sides for simplicity and build up from there.
-
-menu = { "Pizza" => { "pepperoni" => $5, "cheese" => $4, supreme => $10 }, "Sides" => { "Cheesesticks" => $6, "Breadsticks" => $6, "Somestuff" => $7 } }
-
-
-=end
-
-def pizza_size
-  puts "What size?\n1: Small\n2: Medium\n3: Large\n4: Extra Large"
-  print "> "
-  select = gets.chomp.to_i
-    if select == 1
-      return size = 9.99
-    elsif select == 2
-      return size = 12.24
-    elsif select == 3
-      return size = 16.99
-    elsif select == 4
-      return size = 18.74
-    end
-end
+master_order = Array.new
 
 def pizza_menu(order)
   puts "Select an option.\n1: Pepperoni Pizza\n2: Cheese Pizza\n3: Supreme Pizza\n4: Meat Lover's Pizza"#\n5: Custom Pizza"
   print "> "
   select = gets.chomp.to_i
 
-  #build pizza
-  #[price, name, size, toppings]
+  def custom_pizza
+    puts "Select your toppings.\n1: Meats\n2: Veggies\n3: Sauce\n4: Special\n5: Crust"
+    print "> "
 
-def custom_pizza
-  puts "Select your toppings.\n1: Meats\n2: Veggies\n3: Sauce\n4: Special\n5: Crust"
-  print "> "
-
-end
-
-  if select == 1
-    # pizza_size
-    order.push [pizza_size.to_f, "Pepperoni Pizza"]
-  elsif select == 2
-    # pizza_size
-    order.push [pizza_size.to_f, "Cheese Pizza"]
-  elsif select == 3
-    # pizza_size
-    order.push [(pizza_size.to_f + 2), "Supreme Pizza"]
-  elsif select == 4
-    # pizza_size
-    order.push == [(pizza_size.to_f + 2), "Meat Lover's Pizza"]
-  #elsif select == 5
     #Custom pizza Menu
     #add $1.00 for each additional topping
   else
@@ -65,105 +16,94 @@ end
   main_menu(order)
 end
 
-def sides(order)
-  puts "Select a side to add.\n1: Breadsticks\n2: Cheesesticks\n3: Chicken wings"
-  print "> "
-  select = gets.chomp.to_i
-  if select == 1
-    order.push [6.50, "Breadsticks"]
-  elsif select == 2
-    order.push [6.50, "Cheesesticks"]
-  elsif select == 3
-    order.push [8.00, "Wings"]
-  else
+=begin
+
+def add_item_to_order(params)
+  push the item into a master order hash; item => [item name, [array of ingredients], price];
+  iterate over master hash to determine price for subtotal
+end
+
+=end
+
+=begin
+Nest hashes of hashes. Iterate over it in erb to display options; use this for multiple pages. Start with sides for simplicity and build up from there.
+=end
+
+def add_to_order(item, order)
+  order << item
+end
+
+def calc_total(params)
+  money_hash = {
+    "delivery_fee" => 0.0,
+    "subtotal" => 0.0,
+    "tax" => 0.0,
+    "total" => 0.0,
+    "tip" => 0.0
+  }
+
+  #Cost
+  subtotal = 0.00.to_f
+  tax_rate = 0.06
+  subtotal = subtotal.to_f
+  delivery_fee = 2.50
+  puts "\n\n"
+  #puts "Current order is #{order}"
+
+  order.length times do
+    puts "$#{sprintf("%.02f", order[counter][0])} #{order[counter][1]}"
+    subtotal += order[counter][0]
   end
-  main_menu(order)
+
+  puts "\nDelivery Fee: $#{sprintf("%.02f", delivery_fee)}"
+  puts "Subtotal: $#{sprintf("%.02f", subtotal)}"
+  puts "Tax: $#{sprintf("%.02f", subtotal * tax_rate)}"
+  puts "Total: $#{sprintf("%.02f", subtotal * (1 + tax_rate) + delivery_fee)}"
+  puts "\nSuggested Tip: $#{sprintf("%.02f", subtotal * 0.10)}"
+
+  return money_hash
 end
 
-def deserts(order)
-  puts "Select a desert to add.\n1: Cookie\n2: Brownie"
-  print "> "
-  select = gets.chomp.to_i
-  if select == 1
-    order.push [7.00, "Cookie"]
-  elsif select == 2
-    order.push [7.00, "Brownie"]
-  else
-  end
-  main_menu(order)
+def menu_return()
+ menu = {
+         "Pizza" => {
+          "pepperoni" => 5.00,
+          "cheese" => 4.00,
+          "supreme" => 10.00,
+          "custom" => {
+                "size" => {
+                "small" => 9.99,
+                "medium" => 12.24,
+                "large" => 16.99,
+                "extra large" => 18.74
+                          },
+                "meats" => ["pepperoni","sausage", "italian sausage", "bacon", "ham", "beef"],
+                "veggies" => ["peppers", "banana peppers", "olives", "pineapple", "mushrooms"],
+                "crusts" => ["regular", "stuffed", "pretzel", "bacon wrapped"],
+                "sauce" => ["regular", "cheese", "ranch", "BBQ"],
+                "special" => ["three cheese blend", "toothpaste", "legos"],
+                         }
+                       },
+         "Sides" => {
+          "Cheesesticks" => 6.50,
+          "Breadsticks" => 6.50,
+          "Wings" => 8.00
+        },
+         "drinks" => {
+           "Coke (2L)" => 2.99,
+           "Pepsi (2L)" => 2.99
+         },
+         "deserts" => {
+           "Cookie" => 7.00,
+           "Brownie" => 7.00
+         },
+         "extras" => {
+           "Garlic Sauce" => 0.50,
+           "Pizza Sauce" => 0.50,
+           "Ranch Sauce" => 0.50,
+           "Jalapeno peppers" => 0.50,
+           "Anchovies" => 0.50
+               },
+       }
+return menu
 end
-
-def drinks(order)
-  puts "Select a soda to add. Sold as 2 liter bottles.\n1: Coke\n2: Pepsi\n3: Root Beer"
-  print "> "
-  select = gets.chomp.to_i
-  if select == 1
-    order.push [2.99, "Coke (2L)"]
-  elsif select == 2
-    order.push [2.99, "Pepsi (2L)"]
-  elsif select == 3
-    order.push [2.99, "Root Beer (2L)"]
-  else
-  end
-  main_menu(order)
-end
-
-def extras(order)
-  puts "Select an extra to add.\n1: Garlic Sauce\n2: Pizza Sauce\n3: Ranch Sauce\n4: Jalapeno peppers\n5: Anchovies"
-  print "> "
-  select = gets.chomp.to_i
-  if select == 1
-    order.push [0.49, "Garlic Sauce"]
-  elsif select == 2
-    order.push [0.49, "Pizza Sauce"]
-  elsif select == 3
-    order.push [0.49, "Ranch Sauce"]
-  elsif select == 4
-    order.push [0.49, "Jalapeno Peppers"]
-  elsif select == 5
-    order.push [0.49, "Anchovies"]
-  else
-  end
-  main_menu(order)
-end
-
-def main_menu(order)
-puts "What would you like to add to purchase? (Enter number)\n1: Pizza\n2: Sides\n3: Deserts\n4: Drinks\n5: Extras"
-print "> "
-select = gets.chomp.to_i
-
-  if select == 1
-    pizza_menu(order)
-  elsif select == 2
-    sides(order)
-  elsif select == 3
-    deserts(order)
-  elsif select == 4
-    drinks(order)
-  elsif select == 5
-    extras(order)
-  else
-  end
-end
-
-# main_menu(order)
-
-#Cost
-subtotal = 0.00.to_f
-tax_rate = 0.06
-subtotal = subtotal.to_f
-delivery_fee = 2.50
-puts "\n\n"
-#puts "Current order is #{order}"
-counter = 0
-while counter < order.length
-  puts "$#{sprintf("%.02f", order[counter][0])} #{order[counter][1]}"
-  subtotal = subtotal + order[counter][0]
-  counter = counter + 1
-end
-
-# puts "\nDelivery Fee: $#{sprintf("%.02f", delivery_fee)}"
-# puts "Subtotal: $#{sprintf("%.02f", subtotal)}"
-# puts "Tax: $#{sprintf("%.02f", subtotal * tax_rate)}"
-# puts "Total: $#{sprintf("%.02f", subtotal * (1 + tax_rate) + delivery_fee)}"
-# puts "\nSuggested Tip: $#{sprintf("%.02f", subtotal * 0.10)}"
